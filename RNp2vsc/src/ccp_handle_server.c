609f2b2bc31d97b9c66168fa834e1058afebdeb7
MichiBab <Michael.Babic@haw-hamburg.de>
2019-12-26 12:46:52 +0100

    versionnum now reacts to ascii '1' == 49

--- RNp2vsc/src/ccp_handle_server.c
+++ RNp2vsc/src/ccp_handle_server.c
@@ -30,8 +30,9 @@ int readFromSocket(int socket, struct sockaddr_in clientdata){
     
 
 int react_to_package(struct ccp* ccp_data , struct sockaddr_in clientdata , int socket){
-    if(ccp_data->versionnum != 1){
+    if(ccp_data->versionnum != '1'){
         printf("Wrong version from paket detected!\n");
+        printf("we received: %d\n",ccp_data->versionnum);
         return 1;
         }
     char bufip[INET_ADDRSTRLEN];
